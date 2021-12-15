94bc8c42deb0dab8161523ed58b60c922af1cf53
dmb8637 <dmb8637@mail.ru>
2021-12-15 12:01:35 +0300

    67

--- js/calcsum.js
+++ js/calcsum.js
@@ -1,5 +1,19 @@
+	var euroRateCB = 82.83+2;
+	var usdRateCB = 73.47+2;
+
+	function rate(){
+		$("span[id='rateeuro']").each(function(index){
+			$(this).replaceWith('<span id="rateeuro">1€ = ' + euroRateCB + ' ₽</span>');
+		});
+
+		$("span[id='rateusd']").each(function(index){
+			$(this).replaceWith('<span id="rateusd">1$ = ' +  usdRateCB + ' ₽</span>');
+		});
+	}
+
 	$("td[id='s']").each(function(index){
-		var ss = 1.0*parseFloat($(this).text());
+		var euroRateKalinka = 84.5;
+		var ss = parseFloat($(this).text())/euroRateKalinka;
 		
-		$(this).replaceWith('<td class="lc" id="s">' + Math.round(ss) + '</td>');
+		$(this).replaceWith('<td class="lc" id="s">' + Math.round(ss) + ' €</td>');
 	});
